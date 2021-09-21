c022f75c7712050d0b3fee964eac2b78b79ff7a1
anmol choudhary <78426956+Anmol-choudhary-spec@users.noreply.github.com>
2021-09-21 21:35:51 +0530

    adding registration form

--- app/src/main/java/com/example/rentacab/MainActivity.java
+++ app/src/main/java/com/example/rentacab/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.rentacab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
@@ -14,4 +16,9 @@ Button button,button2;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void register(View veiw){
+        Intent intent = new Intent(this,registerActivity.class);
+startActivity(intent);
+    }
+
 }
